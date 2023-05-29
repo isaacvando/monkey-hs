de00a6e98742df0f6d54b76065356e8f4e98e3f9
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2023-05-28 23:43:11 -0500

    add core lexing

--- Lexer.hs
+++ Lexer.hs
@@ -1,53 +1,91 @@
 module Lexer where
-import Prelude hiding (lex)
-import Data.Function ((&))
 
-data Token = Illegal 
-    | Ident String
-    | Int
-    | Assign
-    | Plus
-    | Comma
-    | Semicolon
-    | Lparen
-    | Rparen
-    | Lbrace
-    | Rbrace
-    | Function
-    | Let
-    deriving Eq
+import Data.Char (isDigit)
+import Data.Function ((&))
+import Data.List
+import Prelude hiding (lex)
+
+data Token
+  = Illegal String
+  | Ident String
+  | Int String
+  | Assign
+  | Plus
+  | Comma
+  | Semicolon
+  | Lparen
+  | Rparen
+  | Lbrace
+  | Rbrace
+  | Function
+  | Let
+  | Eof
+  deriving (Eq)
 
 instance Show Token where
-    show t = case t of
-        Illegal -> "ILLEGAL"
-        Ident i -> "IDENT(" ++ i ++ ")"
-        Int -> "INT"
-        Assign -> "="
-        Plus -> "+"
-        Comma -> ","
-        Semicolon -> ";"
-        Lparen -> "("
-        Rparen -> ")"
-        Lbrace -> "{"
-        Rbrace -> "}"
-        Function -> "FUNCTION"
-        Let -> "LET"
-
+  show t = case t of
+    Illegal i -> "ILLEGAL(" ++ i ++ ")"
+    Ident i -> "IDENT(" ++ i ++ ")"
+    Int i -> "INT(" ++ i ++ ")"
+    Assign -> "="
+    Plus -> "+"
+    Comma -> ","
+    Semicolon -> ";"
+    Lparen -> "("
+    Rparen -> ")"
+    Lbrace -> "{"
+    Rbrace -> "}"
+    Function -> "FUNCTION"
+    Let -> "LET"
+    Eof -> "EOF"
 
 lex :: String -> [Token]
 lex input = case nextToken input of
-    (tok, "") -> tok : []
-    (tok, input') -> tok : lex input'
-
+  (tok, "") -> tok : []
+  (tok, input') -> tok : lex input'
 
 nextToken :: String -> (Token, String)
 nextToken input = case input of
-    '=':xs -> (Assign, xs)
-    '+':xs -> (Plus, xs)
-    '(':xs -> (Lparen, xs)
-    ')':xs -> (Rparen, xs)
-    '{':xs -> (Lbrace, xs)
-    '}':xs -> (Rbrace, xs)
-    ',':xs -> (Comma, xs)
-    ';':xs -> (Semicolon, xs)
-    _ -> (Illegal, "")
+  ' ' : xs -> nextToken xs
+  '\t' : xs -> nextToken xs
+  '\n' : xs -> nextToken xs
+  '\r' : xs -> nextToken xs
+  '=' : xs -> (Assign, xs)
+  '+' : xs -> (Plus, xs)
+  '(' : xs -> (Lparen, xs)
+  ')' : xs -> (Rparen, xs)
+  '{' : xs -> (Lbrace, xs)
+  '}' : xs -> (Rbrace, xs)
+  ',' : xs -> (Comma, xs)
+  ';' : xs -> (Semicolon, xs)
+  "" -> (Eof, "")
+  c : _
+    | isIdentChar c -> getIdent input
+    | isDigit c -> getInteger input
+  _ -> (Illegal input, "")
+
+isIdentifier :: String -> Bool
+isIdentifier input = case words input of
+  word : _ -> all isIdentChar word
+  _ -> False
+
+isIdentChar :: Char -> Bool
+isIdentChar c = c `elem` ['a' .. 'z'] ++ ['A' .. 'Z'] ++ ['_']
+
+isInteger :: String -> Bool
+isInteger input = case words input of
+  word : _ -> all isDigit word
+  _ -> False
+
+getInteger :: String -> (Token, String)
+getInteger input =
+  let (num, rest) = span isDigit input
+   in (Int num, rest)
+
+getIdent :: String -> (Token, String)
+getIdent input =
+  let (ident, rest) = span isIdentChar input
+   in case ident of
+        "let" -> (Let, rest)
+        "fn" -> (Function, rest)
+        _ -> (Ident ident, rest)
--- Spec.hs
+++ Spec.hs
@@ -1,16 +1,67 @@
 import Lexer
-import Text.RawString.QQ
-
 import Test.Hspec
+import Text.RawString.QQ
 
 main :: IO ()
 main = hspec $ do
-    describe "lexer" $ do
-        it "lexes symbols" $ do
-            Lexer.lex "=+(){},;" `shouldBe` [Assign, Plus, Lparen, Rparen, Lbrace, Rbrace, Comma, Semicolon]
-            
-        it "lexes a full code block" $ do
-            let 
-                input =  [r|=|] 
-            in 
-                Lexer.lex input `shouldBe` [Assign]
+  describe "lexer" $ do
+    it "lexes symbols" $ do
+      Lexer.lex "=+(){},;" `shouldBe` [Assign, Plus, Lparen, Rparen, Lbrace, Rbrace, Comma, Semicolon]
+
+    it "lexes a full code block" $ do
+      let input =
+            [r|
+            let five = 5;
+            let ten = 10;
+            let add = fn(x, y) {
+            x + y;
+            };
+            let result = add(five, ten);
+            |]
+       in Lexer.lex input
+            `shouldBe` [ Let,
+                         Ident "five",
+                         Assign,
+                         Int "5",
+                         Semicolon,
+                         Let,
+                         Ident "ten",
+                         Assign,
+                         Int "10",
+                         Semicolon,
+                         Let,
+                         Ident "add",
+                         Assign,
+                         Function,
+                         Lparen,
+                         Ident "x",
+                         Comma,
+                         Ident "y",
+                         Rparen,
+                         Lbrace,
+                         Ident "x",
+                         Plus,
+                         Ident "y",
+                         Semicolon,
+                         Rbrace,
+                         Semicolon,
+                         Let,
+                         Ident "result",
+                         Assign,
+                         Ident "add",
+                         Lparen,
+                         Ident "five",
+                         Comma,
+                         Ident "ten",
+                         Rparen,
+                         Semicolon,
+                         Eof
+                       ]
+
+    it "lexes more symbols" $ do
+      let input =
+            [r|
+            !-/*5;
+            5 < 10 > 5;
+            |]
+       in Lexer.lex input `shouldBe` []
